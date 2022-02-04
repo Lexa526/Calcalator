f7cef0ba04a551615758eaf022b18d4c0261e080
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-02-04 21:34:29 +0300

    Update js.js

--- js.js
+++ js.js
@@ -13,7 +13,6 @@ pButton.addEventListener("click", (e) =>{
             if(c<0){
                 let x1 = Math.sqrt(-(c/a))
                 let x2 = -x1
-                alert(", x1 = " + x1 + ", x2 = " + x2)}else {alert("Нет решений(C > 0)")}
+                alert("x1 = " + x1 + ", x2 = " + x2)}else {alert("Нет решений(C > 0)")}
             }
-        }
 })
