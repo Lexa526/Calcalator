34fc30ce07da62cc254c442b836a1451381ab2ea
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-02-04 22:01:56 +0300

    Update js.js

--- js.js
+++ js.js
@@ -9,8 +9,7 @@ pButton.addEventListener("click", (e) =>{
         if (d > 0){
         let x1 = (-b - Math.sqrt(d))/(2 * a)
         let x2 = (-b + Math.sqrt(d))/(2 * a)
-        alert("D = " + d + ", x1 = " + x1 + ", x2 = " + x2)}else {alert("Нет решений(D < 0)")}}else if (a != 0 && b = 0 && c != 0){
-            if(c<0){
+        alert("D = " + d + ", x1 = " + x1 + ", x2 = " + x2)}e lse {alert("Нет решений(D < 0)")}}else if (a != 0 && b != 0 && c < 0){      
                 let x1 = Math.sqrt(-1 * (c/a))
                 let x2 = -1 * x1
                 alert("x1 = " + x1 + ", x2 = " + x2)}else {alert("Нет решений(C > 0)")}
