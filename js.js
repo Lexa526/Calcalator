485e76c994f97155df63a28434cfc422d1fd018d
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-02-04 19:23:04 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+let pButton = document.querySelector(".result")
+pButton.addEventListener("click", (e) =>{
+    console.log(e)
+    let a = document.querySelector(".a").value 
+    let b = document.querySelector(".b").value
+    let c = document.querySelector(".c").value 
+    if(a!=0 & & b!=0 & & c!=0){
+        let d = b**2 -4 * a * c
+        alert(d) 
+    }
+})
