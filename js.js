28620e0696f4ca46a15d15d1b3d4018879e4d736
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-02-04 19:36:24 +0300

    Update js.js

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ pButton.addEventListener("click", (e) =>{
     let a = document.querySelector(".a").value 
     let b = document.querySelector(".b").value
     let c = document.querySelector(".c").value 
-    if(a!=0 && b!=0 && c!=0){
+    if(a!=0){
         let d = b**2 -4 * a * c
         alert(d) 
     }
