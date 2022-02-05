2834f741b2cdbeb16d705139b551c9b52392a444
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-02-05 09:34:56 +0300

    Create js.js

--- js.js
+++ js.js
@@ -6,11 +6,11 @@ pButton.addEventListener("click", (e) =>{
     let c = document.querySelector(".c").value 
     if(a!=0 && b!=0 && c!=0){
         let d = b**2 -4 * a * c 
-        if (d > 0){
+        if(d > 0){
         let x1 = (-b - Math.sqrt(d))/(2 * a)
         let x2 = (-b + Math.sqrt(d))/(2 * a)
         alert("D = " + d + ", x1 = " + x1 + ", x2 = " + x2)}else{alert("Нет решений (D < 0")}
-else if (b = 0){
+        else if(b = 0 && c!=0 && a!=0){
             if(c<0){
                 let x1 = Math.sqrt(-(c/a))
                 let x2 = -x1
