71887ade09185cc56425139b8bdcef8873e0ccd1
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-02-05 15:10:50 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -8,26 +8,26 @@ pButton.addEventListener("click", (e) =>{
     if(c == 0 && b != 0){
     let x1 = 0
     let x2 = -(b/a)
-    alert("x1 = " + num.toFixed(x1) + ", x2 = " + num.toFixed(x2))}
+    alert("x1 = " + x1.toFixed(3) + ", x2 = " + x2.toFixed(3))}
     if(b == 0 && c != 0){
     if(c<0){
       let x1 = Math.sqrt(-(c/a))
       let x2 = -x1
-      alert("x1 = " + num.toFixed(x1) + ", x2 = " + num.toFixed(x2))}
+      alert("x1 = " + x1.toFixed(3) + ", x2 = " + x2.toFixed(3))}
       else {alert("Нет решений(C > 0)")}}  
     if(b==0 && c == 0){
         if (a < 0){
             let x1 = Math.sqrt(-a)
             let x2 = -x1
-            alert("x1 = " + num.toFixed(x1) + ", x2 = " + num.toFixed(x2)) 
+            alert("x1 = " + x1.toFixed(3) + ", x2 = " + x2.toFixed(3)) 
         }else {alert ("Нет решений(a > 0)")}}
     if(b!=0 && c!=0){                
         let d = b**2 -4 * a * c 
         if(d!=0){
         let x1 = (-b - Math.sqrt(d))/(2 * a)
         let x2 = (-b + Math.sqrt(d))/(2 * a)
-        alert("D = " + d + ", x1 = " + num.toFixed(x1) + ", x2 = " + num.toFixed(x2))}else if (D = 0){
+        alert("D = " + d + ", x1 = " + x1.toFixed(3) + ", x2 = " + x2.toFixed(3))}else if (D = 0){
             let x1 = -b /(2 * a)
-            alert("D = " + d + ", x1 = " + num.toFixed(x1))
+            alert("D = " + d + ", x1 = " + x1.toFixed(3))
         }else{alert("Нет решений (D < 0)")}}}else {alert("Нет решений")} 
 })
