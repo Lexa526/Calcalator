543c90226bd8e605736ab91d54061f1217933c69
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-02-05 14:26:29 +0300

    Update js.js

--- js.js
+++ js.js
@@ -4,24 +4,30 @@ pButton.addEventListener("click", (e) =>{
     let a = document.querySelector(".a").value 
     let b = document.querySelector(".b").value
     let c = document.querySelector(".c").value 
-    if(a!=0 && b!=0 && c!=0){
+    if(a!=0){ 
+    if(c == 0){
+    let x1 = 0
+    let x2 = -(b/a)
+    alert("x1 = " + x1 + ", x2 = " + x2)}
+    if(b == 0){
+    if(c<0){
+      let x1 = Math.sqrt(-(c/a))
+      let x2 = -x1
+      alert("x1 = " + x1 + ", x2 = " + x2)}
+      else {alert("Нет решений(C > 0)")}}  
+    if(b==0 && c == 0){
+        if (a < 0){
+            let x1 = Math.sqrt(-a)
+            let x2 = -x1
+            alert("x1 = " + x1 + ", x2 = " + x2) 
+        }else {alert ("Нет решений(a > 0)")}}
+    if(b!=0 c!=0){                
         let d = b**2 -4 * a * c 
-        if(d > 0){
+        if(d!=0){
         let x1 = (-b - Math.sqrt(d))/(2 * a)
         let x2 = (-b + Math.sqrt(d))/(2 * a)
-        alert("D = " + d + ", x1 = " + x1 + ", x2 = " + x2)}else{alert("Нет решений (D < 0)")}}
-    else if(b == 0 && c!=0 && a!=0){
-            if(c<0){
-                let x1 = Math.sqrt(-(c/a))
-                let x2 = -x1
-                alert("x1 = " + x1 + ", x2 = " + x2)}else {alert("Нет решений(C > 0)")}}
-            else if(a!=0 && b!=0 && c == 0){
-                let x1 = 0
-                let x2 = -(b/a)
-                alert("x1 = " + x1 + ", x2 = " + x2)}
-            else if(a < 0 && b == 0 && c == 0){
-                let x1 = Math.sqrt(-a)
-                let x2 = -x
-                alert("x1 = " + x1 + ", x2 = " + x2)}
-            else {alert("Нет решений")} 
+        alert("D = " + d + ", x1 = " + x1 + ", x2 = " + x2)}else if (D = 0){
+            let x1 = -b /(2 * a)
+            alert("D = " + d + ", x1 = " + x1)
+        }else{alert("Нет решений (D < 0)")}}}else {alert("Нет решений")} 
 })
