5aef179e645887953dc65804bdd8745a3bb59b0b
Lexa <86785362+Lexa526@users.noreply.github.com>
2022-02-05 16:19:51 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,6 +1,11 @@
 let pButton = document.querySelector(".result")
 pButton.addEventListener("click", (e) => {
 	console.log(e)
+	function x(x){
+	    if (x != Math.trunc(x)){
+	        return x.toFixed(3) + " (Округлено до 3 чисел после запятой)"
+	    }else return x
+	}
 	let a = document.querySelector(".a").value
 	let b = document.querySelector(".b").value
 	let c = document.querySelector(".c").value
@@ -8,13 +13,13 @@ pButton.addEventListener("click", (e) => {
 		if (c == 0 && b != 0) {
 			let x1 = 0
 			let x2 = -(b / a)
-			alert("x1 = " + x1.toFixed(3) + ", x2 = " + x2.toFixed(3))
+			alert("x1 = " + x(x1) + ", x2 = " + x(x2))
 		}
 		if (b == 0 && c != 0) {
 			if (c < 0) {
 				let x1 = Math.sqrt(-(c / a))
 				let x2 = -x1
-				alert("x1 = " + x1.toFixed(3) + ", x2 = " + x2.toFixed(3))
+				alert("x1 = " + x(x1) + ", x2 = " + x(x2))
 			} else {
 				alert("Нет решений(C > 0)")
 			}
@@ -23,7 +28,7 @@ pButton.addEventListener("click", (e) => {
 			if (a < 0) {
 				let x1 = Math.sqrt(-a)
 				let x2 = -x1
-				alert("x1 = " + x1.toFixed(3) + ", x2 = " + x2.toFixed(3))
+				alert("x1 = " + x(x1) + ", x2 = " + x(x2))
 			} else {
 				alert("Нет решений(a > 0)")
 			}
@@ -33,10 +38,10 @@ pButton.addEventListener("click", (e) => {
 			if (d > 0) {
 				let x1 = (-b - Math.sqrt(d)) / (2 * a)
 				let x2 = (-b + Math.sqrt(d)) / (2 * a)
-				alert("D = " + d + ", x1 = " + x1.toFixed(3) + ", x2 = " + x2.toFixed(3))
+				alert("D = " + d + ", x1 = " + x(x1) + ", x2 = " + x(x2))
 			} else if (d == 0) {
 				let x1 = -b / (2 * a)
-				alert("D = " + d + ", x1 = " + x1.toFixed(3))
+				alert("D = " + d + ", x1 = " + x(x1))
 			} else {
 				alert("Нет решений (D < 0)")
 			}
